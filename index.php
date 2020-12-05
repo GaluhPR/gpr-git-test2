a0f4c5b2b75765d2043ec556699089de1c2d1551
Galuh Prayoga <galuhjb96@gmail.com>
2020-12-05 19:15:06 +0700

    Mengubah tag <p> dan Menambahkan paragraf

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Coba Git Remote</title>
 </head>
 <body>
-    <p>Hello World!!!</p>
+    <h1>Hello World!!!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem perspiciatis vitae exercitationem nulla ipsa at vel quia minus, a recusandae vero mollitia molestiae, reiciendis corrupti obcaecati iusto. Incidunt, molestiae perferendis?</p>
 </body>
 </html>
